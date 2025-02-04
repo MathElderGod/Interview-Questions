dde162640a37cb3529b019fef5fddc8eb40efcf0
Alexander G. Arias <alexanderarias2@hotmail.com>
2025-02-03 17:42:23 -0800

    added new interview question regarding cheaters in a lottery event

---
+++ eliminate_cheaters_question/remove_cheaters.c
@@ -0,0 +1,26 @@
+///////////////////////////////////////////////////////////////////////////////
+/**
+ * 
+ *
+ * I was asked this coding question by EPIC solutions.
+ * Now, at the time, I realize I did a terrible job implementing the solution under 
+ * the stress of the assessment timer. 
+ *
+ * Why don't companies just give you a programming assignment, and just have you turn
+ * it in by a much broader and leniant deadline? A man can dream.....(AI ofcourse is 
+ * the reason why.)
+ *
+ * Anyways, here we go.
+ *
+ * Interview Question:
+ *
+ * You are part of a lottery verification team. So far, there are two seperate lottery events  
+ * going on at the same time. Each event has participants put down their phone numbers on a list. 
+ * Particpants are only allowed to put down one phoner number down, and are no longer allowed to 
+ * participate in any other events for the lottery as a whole. Some particpants however, decided to 
+ * violate the rules, and they have put down their phone numbers in the other list as well. 
+ * Write a program saves each users phone numbers, sorts them, and removes any cheaters from the 
+ * overall lottery population. Return a SORTED list such that it is a list fully comprising of
+ * the phone numbers of individuals that did not cheat. 
+ *
+ * /
